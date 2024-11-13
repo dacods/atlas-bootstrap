d24fcc33a236c35177ca2135c105ec9170ea1649
dacods <dacods.21@gmail.com>
2024-11-12 18:09:44 -0600

    Added a song selection section that uses a dropdown and aside

---
+++ scripts/main.js
@@ -0,0 +1,50 @@
+const genreData = {
+  rock: {
+    imgSrc: 'images/Shed-cover.jpg',
+    albumName: 'Shed',
+    artist: 'Title Fight',
+    year: '2011',
+    spotifyEmbed: `
+      <iframe src="https://open.spotify.com/embed/track/1csPfS6y1zVsZx7SbXJDXJ?utm_source=generator" width="100%" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+    `,
+  },
+  hiphop: {
+    imgSrc: 'images/Alternative_cover.jpg',
+    albumName: 'Alternative Trap',
+    artist: 'Lucki',
+    year: '2013',
+    spotifyEmbed: `
+      <iframe src="https://open.spotify.com/embed/track/0ZFQxmNIOj03o7OMaiPcVX?utm_source=generator" width="100%" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+    `,
+  },
+  indie: {
+    imgSrc: 'images/Marietta.jpg',
+    albumName: 'Summer Death',
+    artist: 'Marietta',
+    year: '2013',
+    spotifyEmbed: `
+      <iframe src="https://open.spotify.com/embed/track/3BepuSCt7eLXv98kbVIp7L?utm_source=generator" width="100%" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+    `,
+  },
+};
+
+
+$(document).ready(function() {
+  $('.dropdown-item').click(function(e) {
+    e.preventDefault();
+    const selectedGenre = $(this).data('genre');
+    const genreText = $(this).text();
+
+    $('#genreDropdown').text(genreText);
+
+    const data = genreData[selectedGenre];
+    if (data) {
+      $('#albumArt').attr('src', data.imgSrc);
+      $('#albumName').text(data.albumName);
+      $('#artistName').text(data.artist);
+      $('#albumYear').text(data.year);
+
+      $('#spotifyPlayer').html(data.spotifyEmbed);
+    }
+  });
+});
